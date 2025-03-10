93d1fdec6150050ff17c7327b8ba5efbf1931c8b
William_Sanchez_Alvarez <williamsanchez@wposs.com>
2025-03-09 19:18:27 -0500

    feat(tasks-menu): implementa la funcionalidad de cierre de sesión con AuthService y navegación

--- src/app/modules/tasks/layouts/components/tasks-menu/tasks-menu.component.ts
+++ src/app/modules/tasks/layouts/components/tasks-menu/tasks-menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../../../../core/services/auth/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-tasks-menu',
@@ -7,12 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasksMenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
   logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
   }
 
 }
